7176b4b1d5513a2f41b3753d80cdf48a6014eebb
Mark Van Moer <mvanmoer@illinois.edu>
2025-03-11 11:23:33 -0500

    Changes to run on the campus cluster

--- containers/start-sandbox.sh
+++ containers/start-sandbox.sh
@@ -3,10 +3,11 @@
 # Wrapper around singularity shell showing how to launch work with a writable 
 # sandbox.
 
-PROJDIR=$HOME/Vis/projects/bowers-ic/
-CLONESDIR=$PROJDIR/src
+PROJDIR=/projects/illinois/las/pol/jwbowers
+CLONESDIR=$PROJDIR/mvanmoer/src
+SANDBOX=$CLONESDIR/mvanmoer-bowers-sandbox
 
-sudo singularity shell --writable \
+apptainer shell --fakeroot --writable \
       --mount type=bind,src=$CLONESDIR/manytestsr,dst=/mnt/manytestsr \
       --mount type=bind,src=$CLONESDIR/test_performance_sims,dst=/mnt/test_performance_sims \
-      mysandbox/
+      $SANDBOX
